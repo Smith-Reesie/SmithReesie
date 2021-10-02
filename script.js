dbecee1bedabaf4e1f07a0824d7d5cc1ae36999c
Maurice Smith <Xmaurice.smith@gmail.com>
2021-10-02 16:33:21 -0400

    added JS and CSS

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Fly Lightly";
 document.querySelector("body").appendChild(h2);
+
+const homeSection = document.querySelector('section.home');
+
+
+//add event listener//
+
+const button = document.querySelector('#btn');
+
+button.addEventListener("click", function() {
+    homeSection.classList.toggle('homeb')
+   })
+
+
